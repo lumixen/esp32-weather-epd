26b5cbd3ab85217b77151f0c88e7a2a9a9129334
Max <max.bodaniuk@gmail.com>
2026-08-06 23:46:14 +0300

    Remove license headers from newly added provider files

--- platformio/src/owm_weather_provider.cpp
+++ platformio/src/owm_weather_provider.cpp
@@ -1,19 +1,3 @@
-/* OpenWeatherMap One Call weather provider for esp32-weather-epd.
- * Copyright (C) 2022-2025  Luke Marzen
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
 #include "config.h"
 
 #if defined(WEATHER_API_OPEN_WEATHER_MAP)
